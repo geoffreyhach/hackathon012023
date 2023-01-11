a61986c3bbe77e9c5a93a24b1ecbc6ce685d0945
Geoffrey Hach <je.suis.geoffrey.hach@gmail.com>
2023-01-11 15:22:22 +0100

    add post car route

--- pages/api/car/index.js
+++
@@ -1,9 +0,0 @@
-import dynamoClient from "../db";
-
-export default async function handler(req, res) {
-    const params = {
-        TableName: "car",
-    };
-    const cars = await dynamoClient.scan(params).promise();
-    res.status(200).json(cars);
-}
