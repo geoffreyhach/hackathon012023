13486fde5898d67546ce89ffcfc84cf345ffe6c1
Geoffrey Hach <je.suis.geoffrey.hach@gmail.com>
2023-01-12 21:50:44 +0100

    change booking page

--- pages/admin/booking.jsx
+++ pages/admin/booking.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import axios from "axios";
 import {
-  DataGrid,
-  GridToolbarContainer,
-  GridToolbarExport,
+    DataGrid,
+    GridToolbarContainer,
+    GridToolbarExport,
 } from "@mui/x-data-grid";
 import { Container, Typography, Box, Button, Stack } from "@mui/material";
 import CachedIcon from "@mui/icons-material/Cached";
@@ -11,111 +11,123 @@ import Dashboard from "../../components/admin/nav/Dashboard";
 import Head from "next/head";
 
 function CustomToolbar() {
-  return (
-    <GridToolbarContainer
-      sx={{ display: "flex", justifyContent: "space-between" }}
-    >
-      <Button onClick={() => window.location.reload()}>
-        <CachedIcon />
-        Actualiser
-      </Button>
-      <GridToolbarExport />
-    </GridToolbarContainer>
-  );
+    return (
+        <GridToolbarContainer
+            sx={{ display: "flex", justifyContent: "space-between" }}
+        >
+            <Button onClick={() => window.location.reload()}>
+                <CachedIcon />
+                Actualiser
+            </Button>
+            <GridToolbarExport />
+        </GridToolbarContainer>
+    );
 }
 
 export default function Booking({ booking, cars }) {
-  const [checkboxSelection, setCheckboxSelection] = useState(true);
+    const [checkboxSelection, setCheckboxSelection] = useState(true);
 
-  const currencyFormatter = new Intl.NumberFormat("fr-FR", {
-    style: "currency",
-    currency: "EUR",
-  });
+    const currencyFormatter = new Intl.NumberFormat("fr-FR", {
+        style: "currency",
+        currency: "EUR",
+    });
 
-  const title = "Réservations";
+    const title = "Réservations";
 
-  return (
-    <>
-      <Head>
-        <title> Réservations | WILDCARS </title>
-      </Head>
-      <Box sx={{ display: "flex" }}>
-        <Dashboard title={title} />
-        <Container style={{ marginTop: "2rem" }}>
-          <Typography variant="h4" gutterBottom>
-            Réservations
-          </Typography>
+    return (
+        <>
+            <Head>
+                <title> Réservations | WILDCARS </title>
+            </Head>
+            <Box sx={{ display: "flex" }}>
+                <Dashboard title={title} />
+                <Container style={{ marginTop: "2rem" }}>
+                    <Typography variant="h4" gutterBottom>
+                        Réservations
+                    </Typography>
 
-          <div
-            style={{ height: "90vh", width: "100%", backgroundColor: "#fff" }}
-          >
-            <DataGrid
-              onCellClick={(e) => console.log(e)}
-              rows={booking}
-              getRowId={(row) => row.pk}
-              loading={!booking}
-              components={{
-                Toolbar: CustomToolbar,
-              }}
-              // editMode="row"
-              checkboxSelection={checkboxSelection}
-              experimentalFeatures={{ newEditingApi: true }}
-              columns={[
-                { field: "pk", headerName: "ID", editable: true, flex: 0.1 },
-                {
-                  field: "model",
-                  headerName: "Véhicule",
-                  editable: true,
-                  flex: 0.25,
-                },
-                {
-                  type: "date",
-                  field: "debut",
-                  headerName: "Date de début",
-                  editable: true,
-                  flex: 0.25,
-                  valueGetter: ({ value }) => value && new Date(value),
-                },
-                {
-                  type: "date",
-                  field: "fin",
-                  headerName: "Date de fin",
-                  editable: true,
-                  flex: 0.2,
-                  valueGetter: ({ value }) => value && new Date(value),
-                },
-                {
-                  field: "amount",
-                  headerName: "Montant",
-                  editable: true,
-                  flex: 0.2,
-                  valueFormatter: ({ value }) =>
-                    currencyFormatter.format(value),
-                },
-                {
-                  type: "boolean",
-                  defaultValue: 1,
-                  field: "isAvailable",
-                  headerName: "Dispo",
-                  editable: true,
-                  flex: 0.13,
-                },
-              ]}
-            />
-          </div>
-        </Container>
-      </Box>
-    </>
-  );
+                    <div
+                        style={{
+                            height: "90vh",
+                            width: "100%",
+                            backgroundColor: "#fff",
+                        }}
+                    >
+                        <DataGrid
+                            onCellClick={(e) => console.log(e)}
+                            rows={booking}
+                            getRowId={(row) => row.pk}
+                            loading={!booking}
+                            components={{
+                                Toolbar: CustomToolbar,
+                            }}
+                            // editMode="row"
+                            checkboxSelection={checkboxSelection}
+                            experimentalFeatures={{ newEditingApi: true }}
+                            columns={[
+                                {
+                                    field: "pk",
+                                    headerName: "ID",
+                                    editable: true,
+                                    flex: 0.1,
+                                },
+                                {
+                                    field: "model",
+                                    headerName: "Véhicule",
+                                    editable: true,
+                                    flex: 0.25,
+                                    valueGetter: ({ row }) => row.car_id,
+                                },
+                                {
+                                    type: "date",
+                                    field: "debut",
+                                    headerName: "Date de début",
+                                    editable: true,
+                                    flex: 0.25,
+                                    valueGetter: ({ value }) =>
+                                        value && new Date(value),
+                                },
+                                {
+                                    type: "date",
+                                    field: "fin",
+                                    headerName: "Date de fin",
+                                    editable: true,
+                                    flex: 0.2,
+                                    valueGetter: ({ value }) =>
+                                        value && new Date(value),
+                                },
+                                {
+                                    field: "amount",
+                                    headerName: "Montant",
+                                    editable: true,
+                                    flex: 0.2,
+                                    valueFormatter: ({ value }) =>
+                                        currencyFormatter.format(value),
+                                },
+                                {
+                                    type: "boolean",
+                                    defaultValue: 1,
+                                    field: "isAvailable",
+                                    headerName: "Dispo",
+                                    editable: true,
+                                    flex: 0.13,
+                                },
+                            ]}
+                        />
+                    </div>
+                </Container>
+            </Box>
+        </>
+    );
 }
 
 export async function getServerSideProps() {
-  const dbHost = process.env.DB_HOST;
-  const res = await axios.get(`http://localhost:3000/api/booking`);
-  const booking = res.data.Items;
-  return {
-    props: {
-      booking,
-    },
-  };
+    const dbHost = process.env.DB_HOST;
+    const res = await axios.get(`http://localhost:3000/api/booking`);
+    const booking = res.data.Items;
+    return {
+        props: {
+            booking,
+        },
+    };
 }
