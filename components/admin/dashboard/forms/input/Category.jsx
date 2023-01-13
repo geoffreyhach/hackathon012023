0bad0c6e0c83aa0c8cb2ca23eabf63a2d047397e
Geoffrey Hach <je.suis.geoffrey.hach@gmail.com>
2023-01-13 11:43:45 +0100

    added category

--- components/admin/dashboard/forms/input/Category.jsx
+++ components/admin/dashboard/forms/input/Category.jsx
@@ -2,49 +2,61 @@ import * as React from "react";
 import { Box, TextField, MenuItem } from "@mui/material";
 
 const categories = [
-  {
-    value: "citadine",
-    label: "Citadine",
-  },
-  {
-    value: "break",
-    label: "Break",
-  },
-  {
-    value: "sport",
-    label: "Sport",
-  },
-  {
-    value: "utilitaire",
-    label: "Utilitaire",
-  },
+    {
+        value: "citadine",
+        label: "Citadine",
+    },
+    {
+        value: "break",
+        label: "Break",
+    },
+    {
+        value: "sport",
+        label: "Sport",
+    },
+    {
+        value: "utilitaire",
+        label: "Utilitaire",
+    },
+    {
+        value: "boat",
+        label: "Boat",
+    },
+    {
+        value: "moto",
+        label: "Moto",
+    },
+    {
+        value: "snowmobile",
+        label: "Snowmobile",
+    },
 ];
 
 export default function Category({ category, handleChange }) {
-  return (
-    <Box
-      component="form"
-      sx={{
-        "& .MuiTextField-root": { m: 1, width: "25ch" },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <div>
-        <TextField
-          id="outlined-select-category"
-          select
-          label="Catégorie"
-          defaultValue=""
-          onChange={handleChange("category")}
+    return (
+        <Box
+            component="form"
+            sx={{
+                "& .MuiTextField-root": { m: 1, width: "25ch" },
+            }}
+            noValidate
+            autoComplete="off"
         >
-          {categories.map((option) => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.label}
-            </MenuItem>
-          ))}
-        </TextField>
-      </div>
-    </Box>
-  );
+            <div>
+                <TextField
+                    id="outlined-select-category"
+                    select
+                    label="Catégorie"
+                    defaultValue=""
+                    onChange={handleChange("category")}
+                >
+                    {categories.map((option) => (
+                        <MenuItem key={option.value} value={option.value}>
+                            {option.label}
+                        </MenuItem>
+                    ))}
+                </TextField>
+            </div>
+        </Box>
+    );
 }
